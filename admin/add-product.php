799ff63c47f9e640140504b1db12a710a6cf7e79
priyanshumay <tiwarimay2002@gmail.com>
2020-07-25 15:31:19 +0530

    added => more admin files

--- admin/add-product.php
+++ admin/add-product.php
@@ -3,9 +3,14 @@
   include('../config/config.php');
   include('../function/function.php');
   include('sidebar.php');
-  
+
+
+
+// Report all errors
+error_reporting(E_ALL);
+
+
 if(!isset($_SESSION['admin'])){
-echo "not admin";
 header('location:login.php');}
 ?>
 <!DOCTYPE html>
@@ -15,7 +20,7 @@ header('location:login.php');}
   <meta name="description" content="ecommerce in php7 , bootstrap4 and mysql">
   <meta name="keywords" content="amazon clone,flpikart clone, php7, mysql, ecommerce website">
   <meta name="author" content="PriyanshuMay,priyanshumay">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
 <title>Add Product</title>	
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
@@ -27,10 +32,39 @@ header('location:login.php');}
 </head>
 <body >
 
-<div id="content" class="p-5 p-md-5 pt-5">
+<?php
+extract($_POST);
+
+if(isset($submit)) {
+  $name = $_POST['name'];
+  $code = $_POST['code'];
+  $cat = $_POST['cat'];
+  $sub_cat = $_POST['sub_cat'];
+  $brand = $_POST['brand'];
+  $supplier = $_POST['supplier'];
+  $qty = $_POST['qty'];
+  $MRP = $_POST['MRP'] ;
+  $cost = $_POST['cost'];
+  $description = $_POST['description'];
+
+  $sql = "INSERT INTO product (name,code,categories,sub_cat,brand,supplier,description,MRP,cost,qty)
+             VALUES('$name','$code','$cat','$sub_cat','$brand','$supplier','$description','$MRP','$cost','$qty')";
+
+  $run=mysqli_query($mysqli, $sql);
+
+  if ($run) {
+      echo "<script>alert('Product has been inserted')</script>";
+      echo "<script>window.open('product_2nd.php','_self')</script>";
+  } else {
+      echo "error";
+  }
+}
+?>
+
+<div id="content" class="pl-5 p-md-5 pt-2">
      
       <div class="row">
-        <div class="col-sm-8 login-section-wrapper">
+        <div class="col-sm-6 login-section-wrapper pl-5 ">
           <div class="login-wrapper">
             <h1 class="login-title">Step 1</h1>
             <form class="form-horizontal" method="post" action="" enctype="multipart/form-data">
@@ -45,7 +79,7 @@ header('location:login.php');}
               <div class="form-group mb-4">
 				<label for="password">Category</label>
         <select id="email" class="form-control" name="cat">
-                                                <option>Select a Category</option>
+                                                <option>Select Something</option>
                                                <?php
                                                 $get_cat = "SELECT * FROM categories";
                                                 $run_cat = mysqli_query($mysqli, $get_cat);
@@ -60,7 +94,28 @@ header('location:login.php');}
                                                 ?>
                                           </select>  
             </div>
+
+        <div class="form-group mb-4">
+				<label for="password">Sub - Category</label>
+        <select id="email" class="form-control" name="sub_cat">
+                                                <option>Select Something</option>
+                                               <?php
+                                                $get_sub_cat = "SELECT * FROM sub_category";
+                                                $run_sub_cat = mysqli_query($mysqli, $get_sub_cat);
+                                                while ($row_sub_cat= mysqli_fetch_array($run_sub_cat)) {
+                                                    $sub_id = $row_sub_cat['sub_id'];
+                                                    $sub_name = $row_sub_cat['sub_name'];
+
+                                                    echo "
+                                                    <option value='$sub_id'>$sub_name</option>
+                                                  ";
+                                                }
+                                                ?>
+                                          </select>  
+            </div>
+
             <div class="form-group mb-4">
+            <label for="email">Brand</label>
             <select class="form-control" name="brand">
                                                 <option>Select a brand</option>
                                                <?php
@@ -80,16 +135,53 @@ header('location:login.php');}
                                           </div> 
 
                                           <div class="form-group mb-4">
+                                          <label for="email">Supplier</label>
+                                          <select class="form-control" name="supplier">
+                                                <option>Select a supplier</option>
+                                               <?php
+
+                                                $get_brand = "SELECT * FROM supplier";
+                                                $run_brand = mysqli_query($mysqli, $get_brand);
+                                                while ($row_brand= mysqli_fetch_array($run_brand)) {
+                                                    $supplier_id = $row_brand['supplier_id'];
+                                                    $supplier_title = $row_brand['supplier_name'];
+
+                                                    echo "
+                                                    <option value='$supplier_id'>$supplier_title</option>
+                                                  ";
+                                                }
+                                                ?>
+                                          </select> 
+                                          </div> 
+
+          
+
+                                  <!--        <div class="col-sm-6 login-section-wrapper pl-5">
+          <div class="login-wrapper">
+            <h1 class="login-title">Step 2</h1> -->
+
+
+            <div class="form-group mb-4">
+                                          <label for="email">Quantity</label>
+                                          <input type="number" name="qty" class="form-control" id="email" placeholder="200" required/> 
                                           </div> 
 
                                           <div class="form-group mb-4">
+                                          <label for="email">Other's Price</label>
+                                          <input type="number" name="MRP"  class="form-control" id="email" placeholder="1000"required/>  
                                           </div> 
 
                                           <div class="form-group mb-4">
+                                          <label for="email">Your's Price</label>
+                                          <input type="number" name="cost"  class="form-control" id="email" placeholder="750"required/>  
                                           </div> 
 
                                           <div class="form-group mb-4">
+                                          <label for="email">Provide A Description</label>
+                                          <textarea type="text" name="description"  class="form-control" id="email" rows="4" cols="50" required/> </textarea>
                                           </div> 
+
+                        
 
               <input type="submit" name="submit" id="submit login" class="btn btn-block login-btn" value="Add Product">
             </form>
@@ -99,132 +191,6 @@ header('location:login.php');}
       </div>     
 </div>    
 
-
-<form class="form-horizontal" method="post" action="product_add.php" enctype="multipart/form-data">
-              <div class="box-body">
-                <!-- FORM START -->
-                <div class="form-group">
-                  <label for="inputEmail3" class="col-sm-2 control-label">Product Title</label>
-                  <div class="col-sm-10">
-                  <input type="text" name="name"  class="form-control" id="inputEmail3" placeholder="Product Name"/> 
-                  </div>
-                </div>
-                <!-- FORM ENDS -->
-                <!-- FORM START -->
-                <div class="form-group">
-                  <label for="inputEmail3" class="col-sm-2 control-label">Category</label>
-                  <div class="col-sm-10">
-                  <select id="cat" class="form-control" name="cat">
-                                                <option>Select a category</option>
-                                               <?php
-                                                $get_cat = "SELECT * FROM categories";
-                                                $run_cat = mysqli_query($mysqli, $get_cat);
-                                                while ($row_cat= mysqli_fetch_array($run_cat)) {
-                                                    $id = $row_cat['cat_id'];
-                                                    $name = $row_cat['cat_name'];
-
-                                                    echo "
-                                                    <option value='$id'>$name</option>
-                                                  ";
-                                                }
-                                                ?>
-                                          </select>  
-                  </div>
-                </div>
-
-                 <div class="form-group" id="cities-container">
-                                 
-                                  </div>
-                <!-- FORM ENDS -->
-
-                   <!-- FORM START -->
-                <div class="form-group">
-                  <label for="inputEmail3" class="col-sm-2 control-label">Brand</label>
-                  <div class="col-sm-10">
-                  <select class="form-control" name="brand">
-                                                <option>Select a brand</option>
-                                               <?php
-
-                                                $get_brand = "SELECT * FROM brand";
-                                                $run_brand = mysqli_query($mysqli, $get_brand);
-                                                while ($row_brand= mysqli_fetch_array($run_brand)) {
-                                                    $brand_id = $row_brand['brand_id'];
-                                                    $brand_title = $row_brand['brand_name'];
-
-                                                    echo "
-                                                    <option value='$brand_id'>$brand_title</option>
-                                                  ";
-                                                }
-                                                ?>
-                                          </select> 
-                  </div>
-                </div>
-                <!-- FORM ENDS -->
-
-                 <!-- FORM START -->
-                <div class="form-group">
-                  <label for="inputEmail3" class="col-sm-2 control-label">Supplier</label>
-                  <div class="col-sm-10">
-                  <select class="form-control" name="supplier">
-                                                <option>Select a supplier</option>
-                                               <?php
-
-                                                $get_brand = "SELECT * FROM supplier";
-                                                $run_brand = mysqli_query($mysqli, $get_brand);
-                                                while ($row_brand= mysqli_fetch_array($run_brand)) {
-                                                    $brand_id = $row_brand['supplier_id'];
-                                                    $brand_title = $row_brand['supplier_name'];
-
-                                                    echo "
-                                                    <option value='$brand_id'>$brand_title</option>
-                                                  ";
-                                                }
-                                                ?>
-                                          </select> 
-                  </div>
-                </div>
-  
-                <div class="form-group">
-                  <label for="inputEmail3" class="col-sm-2 control-label">Price</label>
-                  <div class="col-sm-10">
-                  <input type="text" name="price"  class="form-control" id="inputEmail3" placeholder="Price"/> 
-                  </div>
-                </div>
-         
-                <div class="form-group">
-                  <label for="inputEmail3" class="col-sm-2 control-label">Quantity</label>
-                  <div class="col-sm-10">
-                  <input type="text" name="qty"  class="form-control" id="inputEmail3" placeholder="Qty"/> 
-                  </div>
-                </div>
-       
-                <div class="form-group">
-                  <label for="inputEmail3" class="col-sm-2 control-label">Description</label>
-                  <div class="col-sm-10">
-                  <textarea type="text" name="description"  class="form-control" id="inputEmail3"/></textarea>
-                  </div>
-                </div>
-                <!-- FORM ENDS -->
-
-                 <div class="form_group">
-                            <label for="inputEmail3" class="col-sm-2 control-label">Image</label>
-                  <div class="col-sm-10">
-                            <input type="file" name="cover" >
-                            </div>
-                            </div>
-                            <!-- FORM ENDS -->
-
-               
-               
-              </div>
-              <!-- /.box-body -->
-              <div class="box-footer">
-                
-                <button type="submit" name="submit" class="btn btn-warning pull-right" style="margin: 0 4px">Save</button>
-                <a href="home.php" class="btn btn-default pull-right" >Back</a>
-              </div>
-              <!-- /.box-footer -->
-            </form>
 
 
 </body>

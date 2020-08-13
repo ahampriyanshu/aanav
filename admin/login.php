7aaa47df1718d348e1ba322d8ed49ef7d0292cf0
priyanshumay <tiwarimay2002@gmail.com>
2020-08-14 02:24:59 +0530

    admin panel working

--- admin/login.php
+++ admin/login.php
@@ -1,8 +1,14 @@
 <?php
-  require_once('../essentials/config.php');
-  ?>
+
+require_once('../essentials/config.php');
+if (isset($_SESSION['admin'])) {
+  header('location: index.php');
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta name="description" content="ecommerce in php7 , bootstrap4 and mysql">
@@ -10,34 +16,31 @@
   <meta name="author" content="PriyanshuMay,priyanshumay">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<title>Admin login</title>	
-<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-<link href="https://fonts.googleapis.com/css?family=Karla:400,700&display=swap" rel="stylesheet">
+  <title>Admin login</title>
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Karla:400,700&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="https://cdn.materialdesignicons.com/4.8.95/css/materialdesignicons.min.css">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
   <link rel="stylesheet" href="css/login.css">
 
 </head>
-<body >
-<?php
-extract($_POST);
 
-if(isset($submit))
-{
-  $rs=mysqli_query($connect,"select * from admin where admin_id ='$username' and password='$password'");
-  if(mysqli_num_rows($rs)<1)
-  {
-    $found="N";
+<body>
+  <?php
+  extract($_POST);
+
+  if (isset($submit)) {
+    $rs = mysqli_query($connect, "select * from admin where admin_id ='$username' and password='$password'");
+    if (mysqli_num_rows($rs) < 1) {
+      $found = "N";
+    } else {
+      session_start();
+      $_SESSION["admin"] = $username;
+      header('location:index.php');
+    }
   }
-  else
-  {
-    session_start();
-    $_SESSION["admin"] = $username;
-    header('location:index.php');
-  }
-}
-?>
-<main>
+  ?>
+  <main>
     <div class="container-fluid">
       <div class="row">
         <div class="col-sm-6 login-section-wrapper">
@@ -46,24 +49,23 @@ if(isset($submit))
           </div>
           <div class="login-wrapper my-auto">
             <h1 class="login-title">Hello Admin</h1>
-            <form method="post" name="login_form" action="" >
+            <form method="post" name="login_form" action="">
               <div class="form-group">
-				<label for="email">Email</label> 
-                <input type="text" id="email" class="form-control login_text_box" placeholder="Admin Id" name="username" required/>
+                <label for="email">Email</label>
+                <input type="text" id="email" class="form-control login_text_box" placeholder="Admin Id" name="username" required />
               </div>
               <div class="form-group mb-4">
-				<label for="password">Password</label>
-                <input type="password" name="password" id="password" class="form-control login_text_box" placeholder="enter your passsword"required/>
+                <label for="password">Password</label>
+                <input type="password" name="password" id="password" class="form-control login_text_box" placeholder="enter your passsword" required />
               </div>
-              <?php  
-		  if(isset($found))
-		  {
-		  	echo '<p class="inva" style="font"><center>Invalid Username or password</center></p>';
-		  }
-	?>
+              <?php
+              if (isset($found)) {
+                echo '<p class="inva" style="font"><center>Invalid Username or password</center></p>';
+              }
+              ?>
               <input type="submit" name="submit" id="submit login" class="btn btn-block login-btn" value="Login">
             </form>
-	        </div>
+          </div>
         </div>
         <div class="col-sm-6 px-0 d-none d-sm-block">
           <img src="../img/work.png" alt="login image" class="login-img">
@@ -76,4 +78,5 @@ if(isset($submit))
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
 
 </body>
+
 </html>
--- admin/sidebar.php
+++ admin/sidebar.php
@@ -12,34 +12,34 @@
 			</button>
 		</div>
 		<div class="p-4">
-			<h1><a href="index.html" class="logo">Hello, Admin<span>With Great Power Comes Great Responsibilty</span></a></h1>
+			<h1><a href="index.html" class="logo">Hello, Admin</a></h1>
 			<ul class="list-unstyled components mb-5">
 				<li class="active">
-					<a href="#"><span class="fa fa-home mr-3"></span> Home</a>
+					<a href="#"><i class="fa fa-home mr-3"></i> Home</a>
 				</li>
 				<li>
-					<a href="#"><span class="fa fa-user mr-3"></span>Customers</a>
+					<a href="#"><i class="fa fa-user mr-3"></i>Customers</a>
 				</li>
 				<li>
-					<a href="#"><span class="fa fa-briefcase mr-3"></span>Orders</a>
+					<a href="#"><i class="fa fa-briefcase mr-3"></i>Orders</a>
 				</li>
 				<li>
-					<a href="#"><span class="fa fa-sticky-note mr-3"></span>Billing</a>
+					<a href="#"><i class="fa fa-sticky-note mr-3"></i>Billing</a>
 				</li>
 				<li>
-					<a href="add-product.php"><span class="fa fa-suitcase mr-3"></span>Add New Poduct</a>
+					<a href="add-product.php"><i class="fa fa-suitcase mr-3"></i>Add New Poduct</a>
 				</li>
 				<li>
-					<a href="#"><span class="fa fa-cogs mr-3"></span>Sections</a>
+					<a href="#"><i class="fa fa-cogs mr-3"></i>Sections</a>
 				</li>
 				<li>
-					<a href="#"><span class="fa fa-paper-plane mr-3"></span>Variants</a>
+					<a href="#"><i class="fa fa-paper-plane mr-3"></i>Variants</a>
 				</li>
 				<li>
-					<a href="#"><span class="fa fa-paper-plane mr-3"></span>Categories</a>
+					<a href="#"><i class="fa fa-paper-plane mr-3"></i>Categories</a>
 				</li>
 				<li>
-					<a href="#"><span class="fa fa-paper-plane mr-3"></span>Carousel</a>
+					<a href="#"><i class="fa fa-paper-plane mr-3"></i>Carousel</a>
 				</li>
 
 
@@ -48,17 +48,14 @@
 
 
 			<div class="footer">
-			logout<br>
-					<p>Copyright &copy;<script>
-							document.write(new Date().getFullYear());
-						</script>
-					</p>
+			<p class="text-center" style="font-size:1.2em;"><a href="logout.php"><i class="fas fa-toggle-off"></i>&nbsp;Logout</a></p>
+			<p class="text-center" style="font-size:0.8em;"><i class="fab fa-github"></i> PriyanshuMay &copy; MIT Licensed</p>
 				
 			</div>
 
 		</div>
 	</nav>
-
+	<script src="https://kit.fontawesome.com/77f6dfd46f.js" crossorigin="anonymous"></script>
 	<script src="js/jquery.min.js"></script>
 	<script src="js/popper.js"></script>
 	<script src="js/bootstrap.min.js"></script>
